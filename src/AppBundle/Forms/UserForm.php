c202f88890fe14237efb57c0c96f0a85c8c030c0
Victor <bulddozer86@gmail.com>
2016-05-18 01:19:08 +0300

    added css styles

--- src/AppBundle/Forms/UserForm.php
+++ src/AppBundle/Forms/UserForm.php
@@ -23,22 +23,26 @@ class UserForm extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
+
     $builder->setMethod("POST");
     $builder->setAction('/admin/users/add');
-    $builder->add('login', TextType::class)
-            ->add('password', PasswordType::class)
-            ->add('email', EmailType::class)
-            ->add('first_name', TextType::class)
-            ->add('last_name', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('karma', IntegerType::class)
+    $builder->add('login', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('password', PasswordType::class, array('attr' => array('class' => 'form-control')))
+            ->add('email', EmailType::class, array('attr' => array('class' => 'form-control')))
+            ->add('first_name', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('last_name', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control')))
+            ->add('karma', IntegerType::class, array('attr' => array('class' => 'form-control')))
             ->add('role', EntityType::class, array(
               'class'        => 'AppBundle:Roles',
               'choice_label' => 'name',
               'expanded'     => true,
               'multiple'     => true,
             ))
-            ->add('add', SubmitType::class, array('label' => 'Add'));
+            ->add('add', SubmitType::class, array(
+              'label' => 'Add'
+            ));
+    $builder->setAttribute('class', 'form-horizontal');
 
   }
 
